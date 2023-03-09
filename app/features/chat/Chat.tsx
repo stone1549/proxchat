2823dc55d3e3c6061912708f60632c4b6095a71f
Justin Stone <stone1549@gmail.com>
2023-03-09 17:30:00 -0500

    Chat log will now scroll properly as messages come in

--- app/features/chat/Chat.tsx
+++ app/features/chat/Chat.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useLayoutEffect, useMemo, useRef, useState } from "react";
 import { Banner, HelperText, Surface, useTheme } from "react-native-paper";
 import { useChat } from "../../hooks";
 import styled from "styled-components";
-import { FlatList } from "react-native";
+import { FlatList, ListRenderItemInfo, View } from "react-native";
 import { ChatBubble } from "./ChatBubble";
 import { ChatInput } from "./ChatInput";
-import { isMessage } from "../../domain";
+import { isMessage, Message, PendingMessage } from "../../domain";
+import { NativeSyntheticEvent } from "react-native/Libraries/Types/CoreEventTypes";
+import { NativeScrollEvent } from "react-native/Libraries/Components/ScrollView/ScrollView";
+
+type OnEndReachedFunc =
+  | ((info: { distanceFromEnd: number }) => void)
+  | null
+  | undefined;
+type OnScrollFunc = (event: NativeSyntheticEvent<NativeScrollEvent>) => void;
+type RenderItemFunc = (
+  item: ListRenderItemInfo<Message | PendingMessage>
+) => JSX.Element;
 
 export const Chat: React.FunctionComponent = () => {
   const {
@@ -17,26 +28,52 @@ export const Chat: React.FunctionComponent = () => {
     position,
     error,
   } = useChat();
+  const listRef = useRef<FlatList>(null);
   const theme = useTheme();
+  const [atEndOfList, setAtEndOfList] = useState(false);
+  const listData = [...messages, ...pendingMessages];
 
+  useLayoutEffect(() => {
+    if (atEndOfList && listData.length > 0) {
+      listRef?.current?.scrollToEnd({ animated: true });
+    }
+  }, [listData.length, listRef, atEndOfList]);
+
+  const onReachedEndOfList = useMemo<OnEndReachedFunc>(() => {
+    return () => setAtEndOfList(true);
+  }, [setAtEndOfList]);
+
+  const onScroll = useMemo<OnScrollFunc>(() => {
+    return () => setAtEndOfList(false);
+  }, [setAtEndOfList]);
+
+  const renderItem = useMemo<RenderItemFunc>(() => {
+    return (item) => {
+      const { item: msg } = item;
+      const key = isMessage(msg) ? msg.id : msg.tempId;
+      return (
+        <ChatBubble
+          key={key}
+          message={msg}
+          resendMessage={resendMessage}
+          removePendingMessage={removePendingMessage}
+        />
+      );
+    };
+  }, []);
   return (
     <Styled.Chat theme={theme}>
       <Banner visible={!!error} theme={theme} elevation={3}>
         <Styled.ErrorText type="error">{error}</Styled.ErrorText>
       </Banner>
       <FlatList
-        data={[...messages, ...pendingMessages]}
-        renderItem={(msg) => {
-          const key = isMessage(msg.item) ? msg.item.id : msg.item.tempId;
-          return (
-            <ChatBubble
-              key={key}
-              message={msg.item}
-              resendMessage={resendMessage}
-              removePendingMessage={removePendingMessage}
-            />
-          );
-        }}
+        data={listData}
+        ref={listRef}
+        ListFooterComponent={<Styled.ChatLogFooter />}
+        onScroll={onScroll}
+        onEndReached={onReachedEndOfList}
+        onEndReachedThreshold={0.1}
+        renderItem={renderItem}
       />
       <ChatInput sendMessage={sendMessage} position={position} />
     </Styled.Chat>
@@ -50,5 +87,9 @@ const Styled = {
   `,
   ErrorText: styled(HelperText)`
     text-align: center;
+  `,
+  ChatLogFooter: styled(View)`
+    height: 20px;
+    width: 100%;
   `,
 };
